3e405370542252a3154c1278661d28b46c883b5c
AlAminMiah4274 <alaminmiah4274@gmail.com>
2023-04-07 22:10:38 +0600

    added background img in public folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      backgroundImage: `url("/Img/Rectangle 1.png")`,
+      backgroundRepeat: 'no-repeat',
+      height: '100vh',
+      filter: 'blur(3px)',
+      WebkitFilter: 'blur(3px)',
+
+    }}>
+      Al Amin Miah
     </div>
   );
 }
